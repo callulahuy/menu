83238624c011c944723135f083e4f79fb6082591
unknown <seo.callula.huy@gmail.com>
2020-03-09 01:13:51 +0700

    initial commit

---
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.menu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.navigation.NavigationView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerl;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerl = (DrawerLayout) findViewById(R.id.drawerlayout);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        toggle = new ActionBarDrawerToggle(this, drawerl, R.string.open, R.string.close);
+        navigationView.setNavigationItemSelectedListener(this);
+        drawerl.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.thongtin){
+            Intent intent = new Intent(MainActivity.this,TtActivity.class);
+            startActivity(intent);
+        }
+        return false;
+    }
+}
